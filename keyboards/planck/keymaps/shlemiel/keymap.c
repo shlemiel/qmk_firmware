cb6042cf8f7c38f60cb55889c70ae7a3b523c167
Lune <lune@Mac-mini.local>
2025-04-02 12:04:42 +0900

    fix

--- keyboards/planck/keymaps/shlemiel/keymap.c
+++ keyboards/planck/keymaps/shlemiel/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
     KC_DEL  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_BSLS ,
     KC_TRNS , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_INS  , KC_PSCR , KC_PAUS , KC_APP  , KC_TRNS ,
-    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
+    KC_TRNS, KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
 ),
 
 // higher (sym)
@@ -61,28 +61,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSPC ,
     KC_DEL  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_UNDS , KC_PLUS , KC_LCBR , KC_RCBR , KC_PIPE ,
     KC_TRNS , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_INS  , KC_PSCR , KC_PAUS , KC_APP  , KC_TRNS ,
-    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_HOME , KC_PGDN , KC_PGUP , KC_END
+    KC_TRNS, KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_HOME , KC_PGDN , KC_PGUP , KC_END
 ),
 
-// unused
+// mouse
 [L4] = LAYOUT_planck_grid(
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_TRNS , KC_NO , KC_NO , KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_NO
+    KC_NO   , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_ESC1 ,
+    KC_NO   , KC_BTN1 , KC_BTN2 , KC_BTN3 , KC_BTN4 , KC_BTN5 , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_NO   , KC_NO   ,
+    KC_LSFT , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+    KC_TRNS , KC_RCTL , KC_LGUI , KC_RALT , KC_ACL2 , KC_NO   , KC_NO   , KC_TRNS , KC_LALT , KC_LGUI , KC_LCTL , KC_NO
 ),
 
-// unused
+// mouse
 [L5] = LAYOUT_planck_grid(
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,
-    KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_TRNS , KC_NO , KC_NO , KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_NO
+    KC_NO   , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_ESC1 ,
+    KC_NO   , KC_BTN1 , KC_BTN2 , KC_BTN3 , KC_BTN4 , KC_BTN5 , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_NO   , KC_NO   ,
+    KC_LSFT , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+    KC_TRNS , KC_RCTL , KC_LGUI , KC_RALT , KC_ACL2 , KC_NO   , KC_NO   , KC_TRNS , KC_LALT , KC_LGUI , KC_LCTL , KC_NO
 ),
 
 // adjust
 [L6] = LAYOUT_planck_grid(
-    KC_TRNS , QK_BOOT , DB_TOGG , RGB_TOG , RGB_MOD , RGB_HUI , RGB_HUD , RGB_SAI , RGB_SAD , RGB_VAI , RGB_VAD , KC_DEL  ,
+    KC_TRNS , QK_BOOT , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
