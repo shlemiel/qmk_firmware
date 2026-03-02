d753c29217a71815ff2252aa2837e0767232bd26
Lune <lune@Mac-mini.local>
2026-03-02 13:04:13 +0900

    add build scripts

---
+++ flash_ez.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+qmk compile -kb planck/ez -km shlemiel
+qmk flash -kb planck/ez -km shlemiel
---
+++ flash_rev7.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+qmk compile -kb planck/rev7 -km shlemiel
+qmk flash -kb planck/rev7 -km shlemiel
