c109bee19da5804378978535ab7931b8035c5fad
Lune <lune@Mac-mini.local>
2026-05-15 18:26:07 +0900

    change keymap (inverted first row of numbers)

--- keyboards/planck/keymaps/shlemiel/keymap.c
+++ keyboards/planck/keymaps/shlemiel/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // lower (sym)
 [1] = LAYOUT_planck_grid(
-     KC_ESC  , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_BSPC ,
+     KC_ESC  , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_5    , KC_4    , KC_3    , KC_2    , KC_1    , KC_BSPC ,
      KC_TAB  , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS ,
      KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_GRV  , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_TRNS ,
      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
