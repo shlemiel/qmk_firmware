d8478386f3793b4b28623738e137944f80bc11a9
Lune <lune@Mac-mini.local>
2026-07-20 09:03:49 +0900

    update keymap

--- keyboards/planck/keymaps/shlemiel/keymap.c
+++ keyboards/planck/keymaps/shlemiel/keymap.c
@@ -38,18 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // lower (num)
-[1] = LAYOUT_planck_grid(
+[1] = LAYOUT_tkl_ansi(
      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
-     KC_TAB  , KC_DEL  , KC_NO   , KC_NO   , KC_INS  , KC_LEFT , KC_RGHT , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_BSLS ,
-     KC_TRNS , KC_APP  , KC_PSCR , KC_NO   , KC_PAUS , KC_DOWN , KC_UP   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_TRNS ,
+     KC_TAB  , KC_DEL  , KC_NO   , KC_NO   , KC_INS  , KC_NO   , KC_NO   , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_BSLS ,
+     KC_TRNS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_TRNS ,
      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
 // higher (sym)
-[2] = LAYOUT_planck_grid(
+[2] = LAYOUT_tkl_ansi(
      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSPC ,
-     KC_TAB  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_HOME , KC_END  , KC_UNDS , KC_PLUS , KC_LCBR , KC_RCBR , KC_PIPE ,
-     KC_TRNS , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_PGDN , KC_PGUP , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_TRNS ,
+     KC_TAB  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_UNDS , KC_PLUS , KC_LCBR , KC_RCBR , KC_PIPE ,
+     KC_TRNS , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_TRNS ,
      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
